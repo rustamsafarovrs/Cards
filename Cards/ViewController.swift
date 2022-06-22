678ee69e06103050420b3c6fc49c9930d461f750
rustamsafarovrs <rs.programmer@hotmail.com>
2022-06-22 10:53:11 +0500

    Initial Commit

---
+++ Cards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cards
+//
+//  Created by Сафаров Рустам Далерович on 6/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
